bb90ad6387da06cfa4109b18ac62ba80a642ee14
carlosmehl <carlosmehlcontato@gmail.com>
2026-05-03 21:42:40 -0300

    Primeiro commit - API Alunos

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlunosApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Maria Silva",
+                    Email = "MariaSilva@gmail.com",
+                    Idade = 22
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Joao Sousa",
+                    Email = "JoaoSousa@gmail.com",
+                    Idade = 23
+                }
+                );
+        }
+    }
+}
---
+++ Migrations/20260504003538_PopulaTabelaAluno.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlunosApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaTabelaAluno : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
