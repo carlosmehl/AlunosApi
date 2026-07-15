4303198f33199c067fc4695087b6dda2a8570efe
carlosmehl <carlosmehlcontato@gmail.com>
2026-07-15 18:15:44 -0300

    Adiciona endpoints GET no AlunosController

--- Controllers/AlunosController.cs
+++ Controllers/AlunosController.cs
@@ -1,6 +1,8 @@
-﻿using AlunosApi.Services;
+﻿using AlunosApi.Models;
+using AlunosApi.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AlunosApi.Controllers
 {
@@ -14,5 +16,58 @@ namespace AlunosApi.Controllers
         {
             _alunoService = alunoService;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,"Erro ao obter Alunos.");
+            }
+        }
+
+        [HttpGet("AlunosPorNome")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByName([FromQuery] string nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+
+                if (alunos.Count() == 0)
+                    return NotFound($"Não existem alunos com o critério {nome}");
+
+
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter Alunos.");
+            }
+        }
+
+        [HttpGet("{id:int}", Name="GetAluno")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+
+                if (aluno == null)
+                    return NotFound($"Não existe aluno com o Id= {id}");
+
+
+                return Ok(aluno);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter Aluno.");
+            }
+        }
+
     }
 }
